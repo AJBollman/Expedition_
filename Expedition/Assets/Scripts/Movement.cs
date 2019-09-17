c6069437a502d97b1682ccc8ddc88b4057c29f44
AJBollman <46943559+AJBollman@users.noreply.github.com>
2019-09-16 21:02:53 -0500

    Change the Movement.cs script
    Added slope mechanics

--- Expedition/Assets/Scripts/Movement.cs
+++ Expedition/Assets/Scripts/Movement.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float movementSpeed = 6f;
-    public float sprintSpeed = 9f;
+    public float sprintSpeed = 15f;
     public float jumpForce = 7f;
     public float gravity = 14f;
     public string sprintKey = "left shift";
@@ -19,11 +19,18 @@ public class Movement : MonoBehaviour
 
     private float verticalVelocity;
     private CharacterController controller;
+    private Rigidbody controllerRigidbody;
     private GameObject cam;
+    
+    private Vector3 slopeAngle;
+    RaycastHit hit1, hit2, hit3, hit4, hitOrgin;
+
+    public bool moveAllowed;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        controllerRigidbody = GetComponent<Rigidbody>();
         cam = GameObject.Find("CameraContainer");
         if (!cam) throw new System.Exception("Movement could not find a 'CameraContainer'. Make sure one is placed.");
     }
@@ -37,6 +44,13 @@ public class Movement : MonoBehaviour
         pRot.y = goalAngle;
         transform.rotation = Quaternion.Euler(pRot);
 
+        //checking to see if character is allowed to move
+        if(moveAllowed == true)
+        {
+            controllerRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+        else controllerRigidbody.constraints = RigidbodyConstraints.FreezePosition;
+
 
 
         // ********************************* POSITION
@@ -44,7 +58,7 @@ public class Movement : MonoBehaviour
         if (canMove)
         {
             // Sprint boost.
-            if (Input.GetKeyDown(sprintKey)) { speed = sprintSpeed; }
+            if (Input.GetKey(KeyCode.LeftShift)){ speed = sprintSpeed;}
             else speed = movementSpeed;
 
             // Get forward/backward and side-to-side control inputs.
@@ -61,12 +75,67 @@ public class Movement : MonoBehaviour
         {
             verticalVelocity =- gravity * Time.deltaTime;
             // Apply jump force on [jumpKey].
-            if (Input.GetKeyDown(jumpKey)) verticalVelocity = jumpForce;
+            if (Input.GetKeyDown(jumpKey))
+            {
+                verticalVelocity = jumpForce;
+            }
+           
         }
         else verticalVelocity -= gravity * Time.deltaTime;
+
+        //=============jumping on slope===============
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, controller.height / 2 * 1.5f))
+        {
+            if (hit.normal != Vector3.up)
+            {
+                slopeAngle = hit.normal;
+                if (Input.GetKeyDown(jumpKey))
+                {
+                    verticalVelocity = jumpForce;
+                }
+                else verticalVelocity -= gravity * Time.deltaTime;
+            }
+        }
+
+        slopeCheck();
 
         // Apply movement vectors.
         movement.y = verticalVelocity;
         controller.Move(transform.TransformDirection(movement) * Time.deltaTime);
+        
+    }
+
+    private void slopeCheck()
+    {
+        //========slope sliding===========
+        var ray = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        //ray orgin
+        if (Physics.Raycast(ray, Vector3.down, out hitOrgin))
+        {
+
+        }
+        //line 1
+        if (Physics.Raycast(new Vector3(ray.x + 1, ray.y, ray.z), Vector3.down, out hit1))
+        {
+
+        }
+        //line 2
+        if (Physics.Raycast(new Vector3(ray.x - 1, ray.y, ray.z), Vector3.down, out hit2))
+        {
+
+        }
+        //line 3
+        if (Physics.Raycast(new Vector3(ray.x, ray.y, ray.z + 1), Vector3.down, out hit3))
+        {
+
+        }
+        //line 4
+        if (Physics.Raycast(new Vector3(ray.x, ray.y, ray.z - 1), Vector3.down, out hit4))
+        {
+
+        }
+        //controller.Move(transform.TransformDirection(slopeAngle.x, 0, slopeAngle.z) * Time.deltaTime);
     }
 }
