a17241859ae0fea0616a240312d615c4c844eef7
AJBollman <46943559+AJBollman@users.noreply.github.com>
2019-10-01 17:11:00 -0500

    Fixed nav mesh movement and added ability to pick up items

--- Expedition/Assets/Scripts/NavMesh/NavMeshMovement.cs
+++ Expedition/Assets/Scripts/NavMesh/NavMeshMovement.cs
@@ -7,11 +7,20 @@ public class NavMeshMovement : MonoBehaviour
 {
     public Camera cam;
     public NavMeshAgent agent;
+    private Vector3 Pos;
+    public GameObject home;
+    public GameObject destination;
+
+    public bool failedPath;
+    public bool canMove;
+
+    public int pointHolder = 0;
     public List<Vector3> pointList;
-    
+
     // Update is called once per frame
     void Update()
     {
+        Pos = agent.transform.position;
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -21,8 +30,32 @@ public class NavMeshMovement : MonoBehaviour
             {
                 //get a list of vector 3 points
                 pointList.Add(hit.point);
-                agent.SetDestination(hit.point);
             }
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            canMove = true;
+        }
+
+        if(canMove == true)
+        {
+            if (pointHolder < pointList.Count)
+            {
+                agent.SetDestination(pointList[pointHolder]);
+                if (Vector3.Distance(Pos, pointList[pointHolder]) < 2f)
+                {
+                    pointHolder++;
+                }
+            }
+            else canMove = false;
+        }
+
+        if(failedPath == true)
+        {
+            pointList.Clear();
+            pointHolder = 0;
+            canMove = false;
+            agent.SetDestination(home.transform.position);
         }
     }
     void navMove()
