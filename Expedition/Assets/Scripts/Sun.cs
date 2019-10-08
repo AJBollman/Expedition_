82bed2240c73b4398bbc5912e3322fe7722f4bbe
Detrian Hallem <detrian.hallem@trojans.dsu.edu>
2019-10-08 17:42:33 -0500

    Meta gitignore

---
+++ Expedition/Assets/Scripts/Sun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+    private static bool _created = false;
+    private void Awake()
+    {
+        if (_created) { GameObject.Destroy(this.gameObject); }
+    }
+    private void Start()
+    {
+        _created = true;
+    }
+}
