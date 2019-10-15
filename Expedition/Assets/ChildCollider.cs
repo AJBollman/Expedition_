018d4d3ee8a1c0f5e208d7cbbd76ba5e72f1a6a1
Detrian Hallem <detrian.hallem@trojans.dsu.edu>
2019-10-15 16:47:41 -0500

    Is haz work?

---
+++ Expedition/Assets/ChildCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ChildCollider : MonoBehaviour
+{
+    public bool isA;
+    public bool isMid;
+    void OnCollisionEnter(Collision collision)
+    {
+        transform.parent.GetComponent<Transition>().OnDetect(isA);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        transform.parent.GetComponent<Transition>().OnExit(isA);
+    }
+}
---
+++ Expedition/Assets/Transition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    public Scene AScene;
+    public Scene BScene;
+    private bool isInside;
+    private bool insideA;
+    private bool insideB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnDetect(bool isA)
+    {
+        if (isA) insideA = true;
+        else insideB = true;
+        Debug.Log(isA);
+
+        //if(insideA)
+    }
+
+    public void OnExit(bool isA)
+    {
+        if (isA) insideA = false;
+        else insideB = false;
+        Debug.Log(isA);
+    }
+
+}
