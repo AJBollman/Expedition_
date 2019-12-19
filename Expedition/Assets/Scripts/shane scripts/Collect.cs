5c780487e94346908016371c8fd5546f0c197585
shaneberanek <shane.beranek@trojans.dsu.edu>
2019-12-19 15:20:54 -0600

    collectibles
    made a very basic collectible system

---
+++ Expedition/Assets/Scripts/shane scripts/Collect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour
+{
+    
+    public AudioSource collectSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        ScorringSystem.theScore += 1;
+        Destroy(gameObject);
+    }
+}
---
+++ Expedition/Assets/Scripts/shane scripts/ScorringSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScorringSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    public static int theScore;
+    
+
+    void Update()
+    {
+        scoreText.GetComponent<Text>().text = "Discoveries: " + theScore + " / 10"; 
+    }
+}
