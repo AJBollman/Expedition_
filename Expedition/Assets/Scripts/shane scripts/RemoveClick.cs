1ea5bc18e93c68bd1d5864add7f586c1561f4b6b
shaneberanek <shane.beranek@trojans.dsu.edu>
2019-12-20 11:53:32 -0600

    breakable rocks and bushes
    added the breakable rock at the beginning and added correct audio to the bush and rock breakable prefabs. There is currently a problem where the breakable rock at the beginning will make sounds before clicking on it, and another problem where the audio doesnt work on the objects after the first one has broken.

--- Expedition/Assets/Scripts/shane scripts/RemoveClick.cs
+++ Expedition/Assets/Scripts/shane scripts/RemoveClick.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class RemoveClick : MonoBehaviour
 {
     public GameObject destroyedVersion;
-    public AudioSource clipsound;
+    public AudioSource sound1;
+    public AudioSource sound2;
+    public AudioSource lookingatSound;
+    public bool stoplookingSound = false;
+    public bool backonSound = false;
     public bool timerStart = false;
     public float lifeTime = 10f;
 
@@ -14,19 +18,47 @@ public class RemoveClick : MonoBehaviour
     void OnMouseDown()
     {
         timerStart = true;
+
+        if (stoplookingSound == false)
+        {
+            lookingatSound.Play();
+            stoplookingSound = true;
+            backonSound = true;
+        }
     }
 
+    void OnMouseExit()
+    {
 
+        lookingatSound.Stop();
+        stoplookingSound = false;
+        backonSound = true;
+    }
 
     void OnMouseOver()
     {
-        if (timerStart == true) { lifeTime -= Time.deltaTime; }
+        if (timerStart == true)
+        {
+            
+            lifeTime -= Time.deltaTime;
+            
+        }
 
+
+        if (backonSound == true)
+        {
+            lookingatSound.Play();
+            backonSound = false;
+        }
+        
         if (lifeTime <= 0)
         {
-            clipsound.Play();
+            lookingatSound.Stop();
+            sound1.Play();
+            sound2.Play();
             Instantiate(destroyedVersion, transform.position, transform.rotation);
             Destroy(gameObject);
+            
         }
 
     }
