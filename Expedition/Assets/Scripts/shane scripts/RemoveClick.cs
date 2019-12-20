993aecc5b5eb81a313ab57a75884772b97864456
shaneberanek <shane.beranek@trojans.dsu.edu>
2019-12-19 19:57:18 -0600

    cuttable bushes
    added the ability to make bushes and foliage that can be cut and removed from the terrain

---
+++ Expedition/Assets/Scripts/shane scripts/RemoveClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveClick : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+    public AudioSource clipsound;
+
+
+
+    void OnMouseDown()
+    {
+        clipsound.Play();
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    
+    }
+}
