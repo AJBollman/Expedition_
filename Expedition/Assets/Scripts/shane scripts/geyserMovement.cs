681b370a9d2b2c1f2f818823cc32d3839b59e4ed
shaneberanek <shane.beranek@trojans.dsu.edu>
2020-01-03 23:20:24 -0600

    area near glowworm cave and geysers
    started expanding upon the area near the glow worm cave and added on and off geysers to block the player and traveller from time to time

---
+++ Expedition/Assets/Scripts/shane scripts/geyserMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class geyserMovement : MonoBehaviour
+{
+    
+    public bool timerstart = false;
+    public bool begin = false;
+    public float lifeTime = 0f;
+    public AudioSource geyserSound;
+    public ParticleSystem geyserfoam;
+
+    void Update()
+    {
+
+        if (lifeTime >= 22f)
+        {
+            begin = true;
+        }
+        if (begin == true)
+        {
+            timerstart = true;
+            lifeTime -= Time.deltaTime;
+            geyserfoam.Stop();
+            geyserfoam.enableEmission = false;
+           // geyserSound.Stop();
+        }
+        if (begin == false)
+        {
+            timerstart = false;
+            lifeTime += Time.deltaTime;
+            geyserfoam.enableEmission = true;
+            geyserfoam.Play();
+           // geyserSound.Play();
+        }
+        
+        if (lifeTime <= 0f)
+        {
+            begin = false;
+        }
+
+
+    }
+}
---
+++ Expedition/Assets/Scripts/shane scripts/hideUnhideTime3.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hideUnhideTime3 : MonoBehaviour
+{
+    public bool timerstart = false;
+    public bool begin = false;
+    public float lifeTime = 0f;
+    public AudioSource geyserSound;
+    public GameObject geyserCollider;
+
+
+
+    void Update()
+    {
+
+        if (lifeTime >= 40f)
+        {
+            begin = true;
+        }
+        if (begin == true)
+        {
+            timerstart = true;
+            lifeTime -= Time.deltaTime;
+            geyserCollider.SetActive(false);
+
+            geyserSound.Stop();
+        }
+        if (begin == false)
+        {
+            timerstart = false;
+            lifeTime += Time.deltaTime;
+            geyserCollider.SetActive(true);
+
+            geyserSound.Play();
+        }
+
+        if (lifeTime <= 0f)
+        {
+            begin = false;
+        }
+
+
+    }
+}
