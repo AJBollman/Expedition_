3c0434dc63af4b51967534c7cd557990cc80cc3f
shaneberanek <shane.beranek@trojans.dsu.edu>
2020-01-05 22:53:59 -0600

    rocky bit to left of entrance and falling rocks
    added a little rocky bit to the left of the entrance to the plains, and added and super basic script to allow for cracked surfaces that break when walked on, sending the player into new areas below

---
+++ Expedition/Assets/Scripts/shane scripts/fallingGround.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingGround : MonoBehaviour
+{
+
+    public GameObject destroyedVersion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
