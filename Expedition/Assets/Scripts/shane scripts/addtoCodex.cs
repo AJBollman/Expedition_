92e3cb506d932202dbc2ecabb5f3ec3aaebc2e1e
shaneberanek <shane.beranek@trojans.dsu.edu>
2020-01-20 20:06:10 -0600

    adding an object to the codex
    I made a simple script that adds an object to the codex when clicked. The only one where it doesnt work too well is adding an environmental piece to the codex

---
+++ Expedition/Assets/Scripts/shane scripts/addtoCodex.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addtoCodex : MonoBehaviour
+{
+    public GameObject image;
+    public GameObject NarrativeText;
+    public AudioSource discover;
+
+    void OnMouseDown()
+    {
+       image.SetActive(true);
+       NarrativeText.SetActive(true);
+       discover.Play();
+    }
+}
