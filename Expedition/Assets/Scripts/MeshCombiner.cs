6aae5241d3bdd01d56ceccc4317dd3e9648669fc
shaneberanek <shane.beranek@trojans.dsu.edu>
2020-02-17 09:39:27 -0600

    a few more imposters
    edited the lod system for the geysers

---
+++ Expedition/Assets/Scripts/MeshCombiner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCombiner : MonoBehaviour
+{
+
+    void Start()
+    {
+    
+    }
+
+    void Update()
+    {
+        CombineMeshes();
+    }
+
+    void CombineMeshes()
+    {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+        int i = 0;
+
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+            i++;
+        }
+
+        var meshFilter = transform.GetComponent<MeshFilter>();
+        meshFilter.mesh = new Mesh();
+        meshFilter.mesh.CombineMeshes(combine);
+        GetComponent<MeshCollider>().sharedMesh = meshFilter.mesh;
+        transform.gameObject.SetActive(true);
+
+        transform.localScale = new Vector3(1, 1, 1);
+        transform.rotation = Quaternion.identity;
+        transform.position = Vector3.zero;
+    }
+}
