644c29b5a359d7386b62099c2c74d059365078af
shaneberanek <shane.beranek@trojans.dsu.edu>
2020-03-23 15:07:33 -0500

    sdfbnm
    df

---
+++ Expedition/Assets/Scripts/Utility/TargetFrameRate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFrameRate : MonoBehaviour
+{
+
+    void Start()
+    {
+        Application.targetFrameRate = 60; 
+    }
+
+}
