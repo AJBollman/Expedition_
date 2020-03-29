73e7065d8a280aeaa15baf296c6695e78bd5d4af
shaneberanek <shane.beranek@trojans.dsu.edu>
2020-03-29 17:48:55 -0500

    biome codexs
    started adding the ability to add biomes to the players codex

---
+++ Expedition/Assets/Scripts/UnlockBiomeCodex.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockBiomeCodex : MonoBehaviour
+{
+    public GameObject makeHidden;
+    public AudioSource found;
+    public Collider player;
+
+    private void OnTriggerEnter(Collider player)
+    {
+        makeHidden.SetActive(false);
+        found.Play();
+    }
+}
