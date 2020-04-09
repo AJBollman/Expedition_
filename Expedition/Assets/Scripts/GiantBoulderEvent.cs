1b3540e0d393370601c7e9db71c5d7fc9adf0f12
shaneberanek <shane.beranek@trojans.dsu.edu>
2020-04-09 10:27:24 -0500

    plains hub into overlay
    pretty much finished the plains hub event stuff

---
+++ Expedition/Assets/Scripts/GiantBoulderEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantBoulderEvent : MonoBehaviour
+{
+    public GameObject TravellerUnlock;
+    public GameObject PhoneyUnlock;
+    public GameObject CrumbleUnlock;
+    public GameObject FungalUnlock;
+    public GameObject WispooUnlock;
+    public GameObject GiantBoulder;
+
+    void Update()
+    {
+        if (TravellerUnlock.activeSelf == true && PhoneyUnlock.activeSelf == true && CrumbleUnlock.activeSelf == true && FungalUnlock.activeSelf == true &&
+            WispooUnlock.activeSelf == true)
+        {
+            GiantBoulder.SetActive(false);
+        }
+    }
+}
