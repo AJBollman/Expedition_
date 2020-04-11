ecfb423434e579f8463f1d430dbb11af2a732b54
Detrian Hallem <detrian.hallem@trojans.dsu.edu>
2020-04-10 22:21:00 -0500

    Plains optimization

---
+++ Expedition/Assets/Editor/MultiTerrainEdit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+/// FROM THIS GUY /////////////////////////////
+//https://forum.unity.com/threads/simple-way-to-edit-multiple-terrain-at-once.635080/
+public class MultiTerrainEdit
+    {
+        [MenuItem("Tool/Multi terrain edit")]
+        private static void SetDrawInstanced()
+        {
+            UnityEngine.Terrain[] terrains = Selection.activeGameObject.GetComponentsInChildren<UnityEngine.Terrain>();
+            foreach (var terrain in terrains)
+            {
+                //terrain.drawInstanced = true;
+                terrain.basemapDistance = 200;
+                terrain.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                terrain.reflectionProbeUsage = UnityEngine.Rendering.ReflectionProbeUsage.Off;
+                terrain.detailObjectDensity = 0.5f;
+
+            }
+ 
+            Debug.Log($"Done. {terrains.Length} items.");
+        }
+    }
