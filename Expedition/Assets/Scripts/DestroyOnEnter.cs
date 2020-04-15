bb398e166590778adbff68b24ddcd255b6622270
shaneberanek <shane.beranek@trojans.dsu.edu>
2020-04-15 13:55:50 -0500

    mountain final credits transitin
    put in the final end quest credits transition into the game

---
+++ Expedition/Assets/Scripts/DestroyOnEnter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnEnter : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+    public Collider player;
+
+    private void OnTriggerEnter(Collider player)
+    {
+        Destroy(destroyedVersion);
+    }
+}
