3d507b774b9e10ee84cda66ca0e940378fb60bfe
shaneberanek <shane.beranek@trojans.dsu.edu>
2020-04-17 17:26:51 -0500

    fixed quest image problem through the shane way
    To fix the questimage problem I simply put the objects in each quest image in the overlay. The objects are not seeable from other biomes and the occlusion culling should remove them so everything should work in that regard

--- Expedition/Assets/Scripts/DestroyOnEnter.cs
+++ Expedition/Assets/Scripts/DestroyOnEnter.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class DestroyOnEnter : MonoBehaviour
 {
     public GameObject destroyedVersion;
+    public GameObject QuestTracker;
+    public GameObject thisBlock;
     public Collider player;
 
     private void OnTriggerEnter(Collider player)
     {
         Destroy(destroyedVersion);
+        Destroy(QuestTracker);
+        Destroy(thisBlock);
     }
 }
