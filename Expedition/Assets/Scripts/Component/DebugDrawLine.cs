6e93a19fbf9692aed4cc852a8f763367031cd3ab
Detrian Hallem <detrian.hallem@trojans.dsu.edu>
2020-04-29 10:58:06 -0500

    Quest icon fix
    - define'd out an editor-only script for build
    - uncomitted webdev stuffs

--- Expedition/Assets/Scripts/Component/DebugDrawLine.cs
+++ Expedition/Assets/Scripts/Component/DebugDrawLine.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,3 +16,4 @@ public class DebugDrawLine : MonoBehaviour
         }
     }
 }
+#endif
