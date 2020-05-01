a9a0042b8f310553ddc8a381f2f2ff2efa7889a5
shaneberanek <shane.beranek@trojans.dsu.edu>
2020-05-01 10:52:38 -0500

    added particles to interactibles
    when you click to break rocks and bushes there should be some kind or particle system that plays

--- Expedition/Assets/Scripts/RemoveClick.cs
+++ Expedition/Assets/Scripts/RemoveClick.cs
@@ -19,6 +19,8 @@ public class RemoveClick : MonoBehaviour
     public GameObject UnhideThis2;
     public GameObject UnhideThis3;
 
+    public ParticleSystem dust;
+
 
 
 
@@ -29,6 +31,7 @@ public class RemoveClick : MonoBehaviour
         if (stoplookingSound == false)
         {
             lookingatSound.Play();
+            dust.Play();
             stoplookingSound = true;
             backonSound = true;
         }
@@ -55,7 +58,7 @@ public class RemoveClick : MonoBehaviour
     {
         if (timerStart == true)
         {
-            
+            dust.Play();
             lifeTime -= Time.deltaTime;
         }
 
@@ -63,6 +66,7 @@ public class RemoveClick : MonoBehaviour
         if (backonSound == true)
         {
             //lookingatSound.Play();
+            dust.Play();
             backonSound = false;
         }
 
@@ -71,6 +75,7 @@ public class RemoveClick : MonoBehaviour
         if (lifeTime <= 0)
         {
             lookingatSound.Stop();
+            dust.Play();
             sound1.Play();
             sound2.Play();
           
